bfb1481adfb15bfb7db6e97fdfb6603ac9347df9
ivanr1325 <ivanr@amcoonline.net>
2017-11-16 11:07:39 -0600

    First commit

---
+++ test/test_home_page.rb
@@ -0,0 +1,13 @@
+require 'watir-webdriver'
+require 'watir-webdriver-performance'
+require 'cucumber'
+b = Watir::Browser.new :chrome
+
+b.window.resize_to(1024,768)
+b.window.move_to(0,0)
+b.goto 'http://www.amcoonline.net/extranet/login.php'
+b.text_field(:id => 'username').set 'ivanr'
+b.text_field(:id => 'password').set 'Osama01325'
+b.send_keys :return
+b.span(:class, 'user').wait_until_present(3)
+b.quit
